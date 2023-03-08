a2f5ac776ac8ba8d19c881fb54a084b8d5e570dc
yoon <yoon828990@gmail.com>
2023-03-09 00:13:43 +0900

    Jenkinsfile test

--- backend/src/main/java/doit/Doit/DoitApplication.java
+++ backend/src/main/java/doit/Doit/DoitApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DoitApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("test");
 		SpringApplication.run(DoitApplication.class, args);
 	}
 
