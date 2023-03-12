c89a0125221faef9441a959a38165aab4b23d837
yoon <yoon828990@gmail.com>
2023-03-12 17:44:29 +0900

    backend test

--- backend/src/main/java/doit/Doit/api/controller/TestController.java
+++ backend/src/main/java/doit/Doit/api/controller/TestController.java
@@ -1,11 +1,13 @@
 package doit.Doit.api.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class TestController {
 
     @GetMapping("/api")
